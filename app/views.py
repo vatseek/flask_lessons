af90972716e6d6408918146db2fa1a477566dd0d
vatseek <vadim.gumennyj@gmail.com>
2016-04-02 23:11:55 +0300

    add login

--- app/views.py
+++ app/views.py
@@ -1,7 +1,33 @@
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Miguel'}  # выдуманный пользователь
+    posts = [  # список выдуманных постов
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template(
+            'index.html',
+            title='Home',
+            user=user,
+            posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template(
+            'login.html',
+            title='Sign In',
+            form=form)
